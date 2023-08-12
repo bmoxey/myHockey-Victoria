251c36e734462c76f953274dfa4c6945305c6aee
bmoxey <bmoxey@gmail.com>
2023-08-12 13:52:35 +1000

    Initial Commit

---
+++ myHockey Victoria/myHockey_VictoriaApp.swift
@@ -0,0 +1,17 @@
+//
+//  myHockey_VictoriaApp.swift
+//  myHockey Victoria
+//
+//  Created by Brett Moxey on 12/8/2023.
+//
+
+import SwiftUI
+
+@main
+struct myHockey_VictoriaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
